3f6b4c987fe8d62063bfb376c1cd07a6dc110923
yumojin <yumojin@hotmail.com>
2017-09-16 19:49:04 -0400

    removing my php

--- mojin.php
+++ mojin.php
@@ -13,6 +13,20 @@
 		<h2>About me</h2>
 		<p>My name is Mojin Yu. I am currently an undergraduate student at the University of Rochester, majoring in Digital Media Studies and Psychology. This website is a culmination of my work done in DMS 103.<br>Contact: myu20@u.rochester.edu</p>
 
-		<?PHP include "mojin_body.php"; ?>
+		<h3>Project: Modern Homesickness</h3>
+
+		<p>This project is an interactive animation created from 61 still 
+		frames. Each frame combines real-life photography and a 
+		virtual-environment background.<br>The centerpiece of this project is a teddy bear. To create the rotation effect, the doll was placed on a rotatable platform, then Project: Modern Homesicknessdifferent angles during its 360-degree rotation were captured by a camera.<br>Placed in the back is a series of screenshots of text messages between a daughter and a mother. To create a fuller impression,  this project also contains a background sound effect of smartphone notifications.</p>
+
+		<p>Personal comments: Texting and teddy bear are both something that helps me when I feel homesick. Although serving similar purposes, these are two very different ways – perhaps they also carry different kinds of emotions. So I thought I could put them together as an attempt to combine the complex emotions and see what homesickness “looks like”.</p>
+
+
+		<h3>Project: Adopt, Don't Shop</h3>
+
+		<p>These poster designs aim to promote the idea of pet adoption from animal rescues as a substitution of purchasing from breeders, pet stores, etc. The posters consist of various types of elements: imagery of animals, abstract shapes and slogan text “Adopt. Don’t shop”. Image elements are either from online resources or created using Adobe Illustrator. The main themes and structures of the three designs are kept consistent so that they can work together and leave the audience a stronger impression.</p>
+
+		<p>Personal comments: To me, the part that worked out the best in these posters is the text / font choice. I don’t want the posters to look like forcing people into pet adoption but at the same time, I feel like this issue still needs to be emphasized. That’s why I chose a script-like font (for a friendly tone) but also added a period after the word “adopt” (for determination). I made the text “adopt” significantly larger than “don’t shop” so that “adoption” can be the first thing that catches attention. One note is that the QR code placed at the bottom-right corner of each poster is a working one that directs to a local animal shelter.</p>
+
 	</body>
 </html>
