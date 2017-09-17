49a380163ca41569e0c47184c9e1833a59414de3
sspahija <sspahija@users.noreply.github.com>
2017-09-17 05:48:30 -0400

    added "About Me" section and small code changes

--- sibel.php
+++ sibel.php
@@ -13,59 +13,63 @@
 		</header>
 
 		<article>
+
+			<h2>About Me</h2>
+				<p>I am currently a senior student at the University of Rochester, where I am pursuing a Bachelor of Science Degree in Business with a Finance and Accounting track and am the president of the University of Rochester Investment Banking Program. I am an incoming Investment Banking Analyst at Jefferies LLC, where I interned last summer.</p>
+
 			<h2>Education, Scholarships and Honors</h2>
 
-			<h3>University of Rochester</h3>
-			<ul>
-			<li>Pursuing a B.S. in Business with Finance and Accounting Concentration</li>
-			<li>Recipient of Renaissance and Global, Handler and Davis Scholarships for full tuition and expenses all four years</li>
-			<li>Recipient of selective $3,000 Research and Innovations Grant to conduct independent undergraduate research</li>
-			</ul>
+				<h3>University of Rochester</h3>
+					<ul>
+						<li>Pursuing a B.S. in Business with Finance and Accounting Concentration</li>
+						<li>Recipient of Renaissance and Global, Handler and Davis Scholarships for full tuition and expenses all four years</li>
+						<li>Recipient of selective $3,000 Research and Innovations Grant to conduct independent undergraduate research</li>
+					</ul>
 
-			<h3>United World College of the Adriatic</h3>
-			<ul>
-			<li>Full Academic Scholarship granted from Italian Ministry of International Affairs</li>
-			<li>Activities at school include but not limited to: Editor of school magazine "The Adriatic Times", Social Service with disabled young adults, Kayaking Club</li>
-			</ul>
+				<h3>United World College of the Adriatic</h3>
+					<ul>
+						<li>Full Academic Scholarship granted from Italian Ministry of International Affairs</li>
+						<li>Activities at school include but not limited to: Editor of school magazine "The Adriatic Times", Social Service with disabled young adults, Kayaking Club</li>
+					</ul>
 
 			<h2>Experience</h2>
 
-			<h3>Jefferies LLC</h3>
-			<ul>
-			<li>Interned as a Summer Investment Banking Analyst</li>
-			<li>Worked on various pitches and live deals within the M&A and the Restructuring & Recapitalization groups</li>
-			<li>Industry exposure includes but not limited to: Industrials, Healthcare, Consumer, Energy and Power</li>
-			</ul>
+				<h3>Jefferies LLC</h3>
+					<ul>
+						<li>Interned as a Summer Investment Banking Analyst</li>
+						<li>Worked on various pitches and live deals within the M&A and the Restructuring & Recapitalization groups</li>
+						<li>Industry exposure includes but not limited to: Industrials, Healthcare, Consumer, Energy and Power</li>
+					</ul>
 
-			<h3>University of Rochester - William E. Simon School of Business</h3>
-			<ul>
-			<li>Research Assistant and TA for the MBA class in Operations Management</li>
-			<li>Conducted literature reviews, collected and analyzed data, communicated with current researchers working in similar subjects</li>
-			<li>As a TA helped student prepare for assignments and helped grade exams and class assignments</li>
-			</ul>
+				<h3>University of Rochester - William E. Simon School of Business</h3>
+					<ul>
+						<li>Research Assistant and TA for the MBA class in Operations Management</li>
+						<li>Conducted literature reviews, collected and analyzed data, communicated with current researchers working in similar subjects</li>
+						<li>As a TA helped student prepare for assignments and helped grade exams and class assignments</li>
+					</ul>
 
 			<h2>Leadership</h2>
 
-			<h3>University of Rochester Investment Banking Program</h3>
-			<ul>
-			<li>As the president of the program deligate responsibilities to each mentor</li>
-			<li>Help connect students with Investment Banking professionals</li>
-			<li>Through the mentorship program ensure technical preperation for potential Investment Banking summer positions</li>
-			</ul>
+				<h3>University of Rochester Investment Banking Program</h3>
+					<ul>
+						<li>As the president of the program deligate responsibilities to each mentor</li>
+						<li>Help connect students with Investment Banking professionals</li>
+						<li>Through the mentorship program ensure technical preperation for potential Investment Banking summer positions</li>
+					</ul>
 
 			<h2>International Competitions</h2>
 
-			<h3>International Turkish Language Olympiads</h3>
-			<ul>
-			<li>Chosen ambassador to represent Republic of Kosovo in International Turkish Olympiads</li>
-			<li>Won first place after competing with over 1,000 contestants from 130 different countrie</li>
-			</ul>
+				<h3>International Turkish Language Olympiads</h3>
+					<ul>
+						<li>Chosen ambassador to represent Republic of Kosovo in International Turkish Olympiads</li>
+						<li>Won first place after competing with over 1,000 contestants from 130 different countrie</li>
+					</ul>
 
 			<h2>Skills</h2>
-			<ul>
-			<li>Proficient using Excel, Word, PowerPoint and Internet search tools for topical research, data collection and analysis, drafting and finalizing reports and presentations and for conducting analytics and illustrating findings</li>
-			<li>Bilingual in Albanian and Turkish, Fluent in English and Italian</li>
-			</ul>
+					<ul>
+						<li>Proficient using Excel, Word, PowerPoint and Internet search tools for topical research, data collection and analysis, drafting and finalizing reports and presentations and for conducting analytics and illustrating findings</li>
+						<li>Bilingual in Albanian and Turkish, Fluent in English and Italian</li>
+					</ul>
 
 		</article>
 
