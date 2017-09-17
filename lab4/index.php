337f0a2897e086e0eb4f0b1df7bb6b65df9effd6
zjiang0820 <31747168+zjiang0820@users.noreply.github.com>
2017-09-17 17:36:21 -0400

    Update index.php

--- lab4/index.php
+++ lab4/index.php
@@ -7,6 +7,7 @@
 	</head>
 
 	<body>
+	<div class="container">
 		<header>
 			<h1>Group 9</h1>
 			<div>Student Profiles</div>
@@ -35,32 +36,42 @@
 
 		</nav>
 
+
 		<article>
 
 		<h2>Introducing our group members</h2>
 
+		<div class = zhengliang>
 		<h3>Zhengliang Jiang</h3>
 		
 		<p>My name is Zhengliang. I was born and raised in China. I am currently studying computer science major with an emphasis on human computer interaction at the University of Rochester as a senior. After graduation, I plan to go to a graduate school to pursuit a higher degree.</p>
 		<a href="zhengliang.php"> Read More...</a>
+		</div><!--.zhengliang-->
 
-
+		
+		<div class = mojin>
 		<h3>Mojin Yu</h3>
 
 		<p>My name is Mojin Yu. I am currently an undergraduate student at the University of Rochester, majoring in Digital Media Studies and Psychology. I have great passion in art, music and new media. Currenly I'm working for the History Department as a reserach assitent.</p>
 		<a href="mojin.php"> Read More...</a>
+		</div><!--.mojin-->
+		
 
-
+		<div class = sibel>
 		<h3>Sibel Spahija</h3>
 		
 		<p>I am currently a senior student at the University of Rochester, where I am pursuing a Bachelor of Science Degree in Business with a Finance and Accounting track and am the president of the University of Rochester Investment Banking Program. I am an incoming Investment Banking Analyst at Jefferies LLC, where I interned last summer.</p>
 		<a href="sibel.php"> Read More...</a>
+		</div><!--.sibel-->
+		
 		</article>
+
+
 
 		<footer>
 			<p>This web site is created by Group 9 for CSC 174</p>
 		</footer>
-	
+	</div><!--.container-->
 	</body>
 	
 </html>
