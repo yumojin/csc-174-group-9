6e022b42f54a0d9b1561d66a63ae38aa7bd61ed1
sspahija <sspahija@users.noreply.github.com>
2017-09-17 05:37:27 -0400

    Moved Skills and Abilities to the end for consistency

--- mojin.php
+++ mojin.php
@@ -12,6 +12,19 @@
 		<h2>About me</h2>
 		<p>My name is Mojin Yu. I am currently an undergraduate student at the University of Rochester, majoring in Digital Media Studies and Psychology. <br>Contact: myu20@u.rochester.edu</p>
 
+		<h2>Work Experiences</h2>
+		<h3>RESEARCH ASSISTANT | UNIVERSITY OF ROCHESTER | 2017 JUL - SEP</h3>
+		<p>Job responsibilities include: doing research and collecting historical data; mapping research findings using ArcGIS online; designing posters and map print layout.</p>
+
+		<h3>INTERN | HOUGHTON STREET MEDIA | 2016 JUN - AUG</h3>
+		<p>Job responsibilities include: editing TV Show Positioning documents; organizing project schedules; taking conference notes and writing summaries; translating English documents and business emails; making copies; running errands.</p>
+
+		<h2>Graphic Design Project: Adopt, Don't Shop</h2>
+
+		<p>These poster designs aim to promote the idea of pet adoption from animal rescues as a substitution of purchasing from breeders, pet stores, etc. The posters consist of various types of elements: imagery of animals, abstract shapes and slogan text “Adopt. Don’t shop”. Image elements are either from online resources or created using Adobe Illustrator. The main themes and structures of the three designs are kept consistent so that they can work together and leave the audience a stronger impression.</p>
+
+		<p>Personal comments: To me, the part that worked out the best in these posters is the text / font choice. I don’t want the posters to look like forcing people into pet adoption but at the same time, I feel like this issue still needs to be emphasized. That’s why I chose a script-like font (for a friendly tone) but also added a period after the word “adopt” (for determination). I made the text “adopt” significantly larger than “don’t shop” so that “adoption” can be the first thing that catches attention. One note is that the QR code placed at the bottom-right corner of each poster is a working one that directs to a local animal shelter.</p>
+
 		<h2>Skills and Abilities</h2>
 
 		<ul>
@@ -22,20 +35,6 @@
 			<li>Website building platforms (Weebly, Wix and WordPress</li>
 			<li>ArcGIS online</li>
 		</ul>
-
-		<h2>Work Experiences</h2>
-		<h3>RESEARCH ASSISTANT | UNIVERSITY OF ROCHESTER | 2017 JUL - SEP</h3>
-		<p>Job responsibilities include: doing research and collecting historical data; mapping research findings using ArcGIS online; designing posters and map print layout.</p>
-
-		<h3>INTERN | HOUGHTON STREET MEDIA | 2016 JUN - AUG</h3>
-		<p>Job responsibilities include: editing TV Show Positioning documents; organizing project schedules; taking conference notes and writing summaries; translating English documents and business emails; making copies; running errands.</p>
-
-
-		<h2>Graphic Design Project: Adopt, Don't Shop</h2>
-
-		<p>These poster designs aim to promote the idea of pet adoption from animal rescues as a substitution of purchasing from breeders, pet stores, etc. The posters consist of various types of elements: imagery of animals, abstract shapes and slogan text “Adopt. Don’t shop”. Image elements are either from online resources or created using Adobe Illustrator. The main themes and structures of the three designs are kept consistent so that they can work together and leave the audience a stronger impression.</p>
-
-		<p>Personal comments: To me, the part that worked out the best in these posters is the text / font choice. I don’t want the posters to look like forcing people into pet adoption but at the same time, I feel like this issue still needs to be emphasized. That’s why I chose a script-like font (for a friendly tone) but also added a period after the word “adopt” (for determination). I made the text “adopt” significantly larger than “don’t shop” so that “adoption” can be the first thing that catches attention. One note is that the QR code placed at the bottom-right corner of each poster is a working one that directs to a local animal shelter.</p>
 
 	</body>
 </html>
