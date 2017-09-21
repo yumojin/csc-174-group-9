80077a356aa986c015506921b503cc7fde2abc29
yumojin <yumojin@hotmail.com>
2017-09-20 23:12:36 -0400

    Update mojin.php

--- lab4/mojin.php
+++ lab4/mojin.php
@@ -67,7 +67,7 @@
 						<li>Python, HTML, CSS and JavaScript</li>
 						<li>Audio editing software (Pro Tools and Garage Band)</li>
 						<li>Video editing software (Final Cut Pro and Adobe Premiere)</li>
-						<li>Website building platforms (Weebly, Wix and WordPress</li>
+						<li>Website building platforms (Weebly, Wix and WordPress)</li>
 						<li>ArcGIS online</li>
 					</ul>
 			</aside>
