5d2a0d9f4cd0df92d8a1c9cf451430ba3bda8c6b
sspahija <sspahija@users.noreply.github.com>
2017-09-27 14:36:34 -0400

    changed index page name

--- lab05/zhengliang.php
+++ lab05/zhengliang.php
@@ -18,14 +18,14 @@
   <body>
 
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="test.html">Group Shanghai</a>
+      <a class="navbar-brand" href="index.php">Group Shanghai</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="test.html">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="zhengliang.php">Zhengliang<span class="sr-only">(current)</span></a>
